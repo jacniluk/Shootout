28a7b0bd4e1fc5b823e30f2285cde9bdcc33540b
jacniluk <jacniluk@wp.pl>
2020-11-27 17:23:57 +0100

    Simple map + player + player aiming

---
+++ Shootout/Assets/Scripts/PlayerAiming.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+// Player aiming controller
+public class PlayerAiming : MonoBehaviour
+{
+    // Speed of rotation
+    private const float RotationSpeed = 0.4f;
+
+    // Is player currently aiming
+    private bool isAiming;
+    // Last mouse position in x axis during aiming
+    private float lastXposition;
+
+    // Update
+    private void Update()
+    {
+        UpdateAiming();
+    }
+
+    // Update aiming
+    private void UpdateAiming()
+    {
+        UpdateIsAiming();
+
+        if (isAiming)
+        {
+            UpdateRotation();
+        }
+    }
+
+    // Update state of aiming
+    private void UpdateIsAiming()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isAiming = true;
+            lastXposition = Input.mousePosition.x;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isAiming = false;
+        }
+    }
+
+    // Update player rotation during aiming
+    private void UpdateRotation()
+    {
+        float shift = Input.mousePosition.x - lastXposition;
+        lastXposition = Input.mousePosition.x;
+        RotatePlayer(shift);
+    }
+
+    // Rotate character during aiminig
+    private void RotatePlayer(float shift)
+    {
+        float rotation = shift * RotationSpeed;
+        transform.Rotate(new Vector3(0.0f, rotation, 0.0f));
+    }
+}
