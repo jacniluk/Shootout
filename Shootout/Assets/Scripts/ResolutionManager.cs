0a09c07b05fe937c88d3a9ad260bc63df4830c75
jacniluk <jacniluk@wp.pl>
2020-11-29 20:37:13 +0100

    Resolution manager

---
+++ Shootout/Assets/Scripts/ResolutionManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+// Resolution manager for scaling
+public class ResolutionManager : MonoBehaviour
+{
+    // Start
+    private void Start()
+    {
+        SetCameraFieldOfView();
+    }
+
+    // Set camera FOV depending on device screen width
+    private void SetCameraFieldOfView()
+    {
+        float normalAspectRatio = 1920.0f / 1080.0f;
+        float aspectRatio = (float)Screen.height / Screen.width;
+        float factor = aspectRatio / normalAspectRatio;
+        Camera.main.fieldOfView *= factor;
+    }
+}
